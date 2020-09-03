9ba5ca359aaf1ee993e5274d7d632c7c1ed78be6
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-09-03 16:39:22 +0800

    integrate react router dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import Login from './components/Login';
 import Product from './components/Product';
 import MainNav from './components/MainNav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'; 
 
 const App = () => {
     return (
-      <div>
+      <Router>
       <MainNav />
-        <Login />
-        <Product />
-      </div>
+      <Switch>
+
+      <Route path='/login'>
+      <Login />
+      </Route>
+
+      <Route path="/">
+      <Product />
+      </Route>
+
+      </Switch>
+      </Router>
     );
   }
 
