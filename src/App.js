c53d60f2a1670c897f1ab36e1e7ffeabcd6cb0c5
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-09-03 16:16:01 +0800

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Login from './components/Login';
 import Product from './components/Product';
+import MainNav from './components/MainNav';
 
 const App = () => {
     return (
       <div>
+      <MainNav />
         <Login />
         <Product />
       </div>
     );
   }
 
+
+export default App;
